011f34a546038cc7b62d3cc132927b6dfa8445ef
Joaquin Daneri <joaquindaneri@gmail.com>
2020-08-21 18:47:25 -0300

    version5

--- notifications.php
+++ notifications.php
@@ -1,5 +1,5 @@
 <?php
-// SDK de Mercado Pago
+    // SDK de Mercado Pago
     require __DIR__ .  '/vendor/autoload.php';
     
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
@@ -18,5 +18,7 @@
             $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
             break;
     }
-
+echo "Hola Mundo"
+echo $payment;
+echo $plan;
 ?>
