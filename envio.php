500e177fb7dc4754f8768d412856b345d2630b7a
Joaquin Daneri <joaquindaneri@gmail.com>
2020-08-21 18:54:39 -0300

    version5

--- envio.php
+++ envio.php
@@ -73,7 +73,7 @@ $preference->save();
 ?>
 <form action="/procesar-pago" method="POST">
   <script
-   src="https://www.mercadopago.com/integrations/v1/web-payment-checkout.js"
+   src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
    data-preference-id="<?php echo $preference->id; ?>">
   </script>
 </form>
