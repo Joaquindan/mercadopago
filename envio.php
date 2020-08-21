c2c30d5d755a061cf5de5576910e955c9f022ea5
Joaquin Daneri <joaquindaneri@gmail.com>
2020-08-21 18:16:28 -0300

    version2

--- envio.php
+++ envio.php
@@ -14,6 +14,7 @@ require __DIR__ .  '/vendor/autoload.php';
 
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
@@ -72,7 +73,7 @@ $preference->save();
 ?>
 <form action="/procesar-pago" method="POST">
   <script
-   src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+   src="https://www.mercadopago.com/integrations/v1/web-payment-checkout.js"
    data-preference-id="<?php echo $preference->id; ?>">
   </script>
 </form>
