4d21ddf02570f7061d65b0daecd5456ef20edbf3
Joaquin Daneri <joaquindaneri@gmail.com>
2020-08-24 11:56:03 -0300

    version28

--- notifications.php
+++ notifications.php
@@ -4,6 +4,7 @@
     require __DIR__ .  '/vendor/autoload.php';
     
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+    echo $_POST["type"];
 
     switch($_POST["type"]) {
             case "payment":
